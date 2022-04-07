1566d0fa9650b74c14619247bcc33bd79831cf34
1234Sarvesh <103194719+1234Sarvesh@users.noreply.github.com>
2022-04-07 18:35:59 +0530

    Add files via upload

---
+++ R ProgrammingQuestion.R
@@ -0,0 +1,15 @@
+for (n in 1:100){
+if (n %% 3 == 0 & n%%5 ==0) {print("FizzBuzz")}
+else if (n %% 3 == 0) {print("Fizz")} 
+else if (n %% 5 == 0) {print("Buzz")}
+else print(n)
+}
+
+
+
+print("Sequence of numbers from 20 to 50:")
+print(seq(20,50))
+print("Mean of numbers from 20 to 60:")
+print(mean(20:60))
+print("Sum of numbers from 51 to 91:")
+print(sum(51:91))
